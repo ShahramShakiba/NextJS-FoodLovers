aef7c8167cee4d754be67476147789b6fe3dc77a
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-30 22:01:19 +0330

    Styling NextJS Project Using CSS-Modules

--- NextLevel-Food/components/MainHeader.js
+++ NextLevel-Food/components/MainHeader.js
@@ -1,22 +1,23 @@
 import Link from 'next/link';
 import logoImg from '@/assets/logo.png';
+import classes from './MainHeader.module.css';
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
         <img src={logoImg.src} alt="A plate with food on it!" />
         NextLevel Food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href='/meals'> Browse Meals </Link>
+            <Link href="/meals"> Browse Meals </Link>
           </li>
 
           <li>
-            <Link href='/community'> Community </Link>
+            <Link href="/community"> Community </Link>
           </li>
         </ul>
       </nav>
