c3295bc28d957349c6675a75ca42a8e6e66cb025
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-30 20:56:52 +0330

    Reformating the entire App codes

--- NextLevel-Food/app/page.js
+++ NextLevel-Food/app/page.js
@@ -1,14 +1,9 @@
-import Link from 'next/link';
-import Header from '@/components/header';
-
 export default function Home() {
   return (
     <main>
-      <Header />
-
-      <p>
-        <Link href="/about">About Us</Link>
-      </p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
--- NextLevel-Food/components/header.js
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-    </>
-  );
-}
