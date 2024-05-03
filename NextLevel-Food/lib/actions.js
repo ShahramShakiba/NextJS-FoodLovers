71ed5522f7661004ee83149dfd7b8a69b372f6ff
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-03 16:51:13 +0330

    Adding Server-Side Input Validation

--- NextLevel-Food/lib/actions.js
+++ NextLevel-Food/lib/actions.js
@@ -2,6 +2,10 @@
 import { saveMeal } from './meals';
 import { redirect } from 'next/navigation';
 
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
 //a fn that's execute on the server | Server-Action
 export async function shareMeal(formData) {
   //get input-field "name"
@@ -13,6 +17,19 @@ export async function shareMeal(formData) {
     creator: formData.get('name'),
     creator_email: formData.get('email'),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !email.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error('Invalid input!');
+  }
 
   await saveMeal(meal);
 
